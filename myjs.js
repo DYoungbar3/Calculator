560fa9558ffcc35697e4df82575b3a902c1f5214
Daniel Youngbar <d.youngbar928@gmail.com>
2022-11-20 10:30:27 -0800

    Add functions for calc basics

---
+++ myjs.js
@@ -0,0 +1,35 @@
+function add(...args) {
+    return args.reduce(function(first, second) {
+        return first + second;
+    });
+}
+
+function subtract(...args) {
+    return args.reduce(function(first, second) {
+        return first - second;
+    });
+}
+
+function multiply(...args) {
+    return args.reduce(function(first, second) {
+        return first * second;
+    });
+}
+
+function divide(...args) {
+    return args.reduce(function(first, second) {
+        return first / second;
+    });
+}
+
+function operate(operator, first, second) {
+    if (operator === '+') {
+        return add(first, second);
+    } else if (operator === '-') {
+        return subtract(first, second);
+    } else if (operator ==='*') {
+        return multiply(first, second);
+    } else if (operator === '/') {
+        return divide(first, second);
+    };
+}
